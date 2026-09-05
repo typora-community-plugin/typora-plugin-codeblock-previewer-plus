8bf52ed7b9c7f2bfa69c36b60b569bf7bbb61f90
plylrnsdy <plylrnsdy@163.com>
2026-09-05 13:10:53 +0800

    feat(settings): add plugin settings tab for configurable preview languages

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,40 @@
 import './style.scss'
-import { app, Plugin, CodeblockPostProcessor } from '@typora-community-plugin/core'
+import { app, Plugin, CodeblockPostProcessor, PluginSettings, SettingTab, SettingItem } from '@typora-community-plugin/core'
 import { PreviewFloatingView, openPreviewFloatingWindow } from './floating-view'
 
 
-export default class extends Plugin {
+interface PluginSettingsData {
+  langs: string[]
+}
+
+const DEFAULT_SETTINGS: Partial<PluginSettingsData> = {
+  langs: ['flow', 'mermaid', 'sequence']
+}
+
+export default class CodeblockPreviewerPlus extends Plugin<Partial<PluginSettingsData>> {
   onload(): void {
     this.register(
       app.viewManager.registerView(PreviewFloatingView.type, (leaf) => new PreviewFloatingView(leaf)))
 
+    const settings = new PluginSettings(this.app, this.manifest, { version: 1 })
+    settings.setDefault(DEFAULT_SETTINGS)
+    this.registerSettings(settings)
+
+    this.registerSettingTab(new SettingsTab(this))
+
     this.registerMarkdownPostProcessor(
-      new PreviewButtonProcessor())
+      new PreviewButtonProcessor(() => (this.settings?.get('langs') as string[]) || DEFAULT_SETTINGS.langs!))
   }
 }
-
-const LANGS = ['mermaid']
 
 interface PostProcessorContextLike {
   containerEl: HTMLElement
 }
 
 class PreviewButtonProcessor extends CodeblockPostProcessor {
-  override lang: string[] = [...LANGS]
-
-  constructor() {
+  constructor(private getLangs: () => string[]) {
     super()
+    this.lang = [...this.getLangs()]
     this.button = {
       text: '<i class="fa fa-external-link"></i>',
       title: 'Open preview in floating window',
@@ -42,6 +53,44 @@ class PreviewButtonProcessor extends CodeblockPostProcessor {
   }
 }
 
-type MarkdownRendererWithRegister = {
-  register(processor: CodeblockPostProcessor): () => void
+class SettingsTab extends SettingTab {
+
+  get name(): string {
+    return 'Codeblock Previewer Plus'
+  }
+
+  constructor(private plugin: CodeblockPreviewerPlus) {
+    super()
+  }
+
+  onshow(): void {
+    this.render()
+  }
+
+  render(): void {
+    const settings = this.plugin.settings as PluginSettings<PluginSettingsData>
+
+    this.addSettingTitle('Supported Languages')
+
+    this.addSetting((setting: SettingItem) => {
+      setting.addName('LANGS')
+      setting.addDescription('Comma-separated list of codeblock languages to enable preview button for (e.g. flow,mermaid,sequence)')
+      setting.addText((input: HTMLInputElement) => {
+        input.value = settings.get('langs').join(',')
+        input.onchange = () => {
+          const langs = input.value.split(',').map(s => s.trim()).filter(Boolean)
+          settings.set('langs', langs)
+        }
+      })
+    })
+
+    this.addSetting((setting: SettingItem) => {
+      setting.addButton((button: HTMLButtonElement) => {
+        button.textContent = 'Apply & Reload'
+        button.onclick = () => {
+          location.reload()
+        }
+      })
+    })
+  }
 }
